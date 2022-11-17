6d6547b1312c32b0726aa84f24f794bdad209dbc
nokkvidan <nokkvidan@gmail.com>
2022-11-17 06:21:45 -0500

    lint the package

--- tests/testthat/test-spectral.R
+++ tests/testthat/test-spectral.R
@@ -2,7 +2,7 @@ test_that(
   "The accel_plot() returns a ggplot object. spectral",
   {
     data(ukb_accel)
-    p = ukb_accel[1:100, ] |>
+    p <- ukb_accel[1:100, ] |>
       spectral_signature(take_log = TRUE) |>
       accel_plot()
     expect_true(inherits(p, "gg"))
