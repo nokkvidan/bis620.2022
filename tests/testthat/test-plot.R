6190008e624d1312bbcc81ac144408dfd8c3f65e
nokkvidan <nokkvidan@gmail.com>
2022-11-17 06:21:45 -0500

    lint the package

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -2,7 +2,7 @@ test_that(
   "The accel_plot() returns a ggplot object. time column",
   {
     data(ukb_accel)
-    p = accel_plot(ukb_accel[1:100,])
+    p <- accel_plot(ukb_accel[1:100, ])
     expect_true(inherits(p, "gg"))
   }
 )
@@ -13,7 +13,7 @@ test_that(
     data(ukb_accel)
     data_freq <- ukb_accel
     colnames(data_freq)[1] <- "freq"
-    p = accel_plot(data_freq[1:100, ])
+    p <- accel_plot(data_freq[1:100, ])
     expect_true(inherits(p, "gg"))
   }
 )
@@ -30,7 +30,7 @@ test_that(
   "The accel_plot() is correct for time-series data.",
   {
     data(ukb_accel)
-    p = accel_plot(ukb_accel[1:100,])
+    p <- accel_plot(ukb_accel[1:100, ])
     vdiffr::expect_doppelganger("first-100-samples", p)
   }
 )
