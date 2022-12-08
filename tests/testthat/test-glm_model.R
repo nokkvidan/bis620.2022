c4ded5f38c9b4bf03eaefcd53f27c7b1b655fa14
nokkvidan <nokkvi.ellidason@yale.edu>
2022-12-08 05:12:43 -0500

    lintr

--- tests/testthat/test-glm_model.R
+++ tests/testthat/test-glm_model.R
@@ -6,12 +6,12 @@ test_that(
   "The glm_model() runs with no errors with test data and stepAIC optimization",
   {
     df <- read.csv("/Users/elisaloy/Desktop/diabetes_binary.csv")
-    sample <- sample(c(1, 2), nrow(df), replace=TRUE, prob=c(0.8, 0.2))
+    sample <- sample(c(1, 2), nrow(df), replace = TRUE, prob = c(0.8, 0.2))
     train <- df[sample == 1, ]
     test <- df[sample == 2, ]
-    glm.model <- glm_model(train, "Diabetes_binary", test, optimize = "stepAIC")
-    expect_true(inherits(glm.model, "list"))
-    expect_true(inherits(glm.model[[7]], "roc"))
+    glm_fit <- glm_model(train, "Diabetes_binary", test, optimize = "stepAIC")
+    expect_true(inherits(glm_fit, "list"))
+    expect_true(inherits(glm_fit[[7]], "roc"))
   }
 )
 
@@ -22,9 +22,9 @@ test_that(
     sample <- sample(c(1, 2), nrow(df), replace=TRUE, prob=c(0.8, 0.2))
     train <- df[sample == 1, ]
     test <- df[sample == 2, ]
-    glm.model <- glm_model(df, "Diabetes_binary", test, optimize = "manual")
-    expect_true(inherits(glm.model, "list"))
-    expect_true(inherits(glm.model[[7]], "roc"))
+    glm_fit <- glm_model(df, "Diabetes_binary", test, optimize = "manual")
+    expect_true(inherits(glm_fit, "list"))
+    expect_true(inherits(glm_fit[[7]], "roc"))
   }
 )
 
@@ -33,7 +33,7 @@ test_that(
   "The glm_model() runs with no errors with no data and no optimization",
   {
     df <- read.csv("/Users/elisaloy/Desktop/diabetes_binary.csv")
-    glm.model <- glm_model(df, "Diabetes_binary")
-    expect_true(inherits(glm.model, "glm"))
+    glm_fit <- glm_model(df, "Diabetes_binary")
+    expect_true(inherits(glm_fit, "glm"))
   }
 )
--- tests/testthat/test-vis_2vars.R
+++ tests/testthat/test-vis_2vars.R
@@ -6,7 +6,7 @@ test_that(
   "The vis_2vars() returns a ggplot object for: factor v cont",
   {
     df <- read.csv("/Users/elisaloy/Desktop/diabetes_binary.csv")
-    p <- vis_2vars(df, 'Diabetes_binary', 'BMI')
+    p <- vis_2vars(df, "Diabetes_binary", "BMI")
     expect_true(inherits(p, "gg"))
   }
 )
@@ -15,7 +15,7 @@ test_that(
   "The vis_2vars() returns a ggplot object for: cont v factor",
   {
     df <- read.csv("/Users/elisaloy/Desktop/diabetes_binary.csv")
-    p <- vis_2vars(df, 'BMI', 'Diabetes_binary')
+    p <- vis_2vars(df, "BMI", "Diabetes_binary")
     expect_true(inherits(p, "gg"))
   }
 )
@@ -24,7 +24,7 @@ test_that(
   "The vis_2vars() returns a ggplot object for: factor v factor",
   {
     df <- read.csv("/Users/elisaloy/Desktop/diabetes_binary.csv")
-    p <- vis_2vars(df, 'Diabetes_binary', 'Smoker')
+    p <- vis_2vars(df, "Diabetes_binary", "Smoker")
     expect_true(inherits(p, "gg"))
   }
 )
@@ -34,8 +34,7 @@ test_that(
   "The vis_2vars() returns a ggplot object for: cont v cont",
   {
     df <- read.csv("/Users/elisaloy/Desktop/diabetes_binary.csv")
-    p <- vis_2vars(df, 'BMI', 'MentHlth')
+    p <- vis_2vars(df, "BMI", "MentHlth")
     expect_true(inherits(p, "gg"))
   }
 )
-
