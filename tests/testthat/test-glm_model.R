186060be77c7a4d6ce95aebfca29e0a54420aa1c
nokkvidan <nokkvi.ellidason@yale.edu>
2022-12-08 05:13:17 -0500

    lintr 2

--- tests/testthat/test-glm_model.R
+++ tests/testthat/test-glm_model.R
@@ -19,7 +19,7 @@ test_that(
   "The glm_model() runs with no errors with test data and manual optimization",
   {
     df <- read.csv("/Users/elisaloy/Desktop/diabetes_binary.csv")
-    sample <- sample(c(1, 2), nrow(df), replace=TRUE, prob=c(0.8, 0.2))
+    sample <- sample(c(1, 2), nrow(df), replace = TRUE, prob = c(0.8, 0.2))
     train <- df[sample == 1, ]
     test <- df[sample == 2, ]
     glm_fit <- glm_model(df, "Diabetes_binary", test, optimize = "manual")
