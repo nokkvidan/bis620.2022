0ec4b68b06d55fe0c77cf08d96e698cad8e9871a
nokkvidan <nokkvi.ellidason@yale.edu>
2022-12-08 05:12:43 -0500

    lintr

--- tests/testthat/test-boost_model.R
+++ tests/testthat/test-boost_model.R
@@ -4,12 +4,12 @@ test_that(
   "The boost_model() runs with test data",
   {
     df <- read.csv("/Users/elisaloy/Desktop/diabetes_binary.csv")
-    sample <- sample(c(1, 2), nrow(df), replace=TRUE, prob=c(0.8, 0.2))
+    sample <- sample(c(1, 2), nrow(df), replace = TRUE, prob = c(0.8, 0.2))
     train <- df[sample == 1, ]
     test <- df[sample == 2, ]
-    xg.model <- boost_model("Diabetes_binary", train, test)
-    expect_true(inherits(xg.model, "list"))
-    expect_true(inherits(xg.model[[6]], "roc"))
+    xg_fit <- boost_model("Diabetes_binary", train, test)
+    expect_true(inherits(xg_fit, "list"))
+    expect_true(inherits(xg_fit[[6]], "roc"))
   }
 )
 
@@ -17,7 +17,7 @@ test_that(
   "The boost_model() runs with no test data",
   {
     df <- read.csv("/Users/elisaloy/Desktop/diabetes_binary.csv")
-    xg.model <- boost_model("Diabetes_binary", df)
-    expect_true(inherits(xg.model, "xgb.Booster"))
+    xg_fit <- boost_model("Diabetes_binary", df)
+    expect_true(inherits(xg_fit, "xgb.Booster"))
   }
 )
