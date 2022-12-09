0284f4829e331c3169db74f5d01f241f08222472
Elisa Loy <37096340+loye16@users.noreply.github.com>
2022-12-09 04:01:59 -0500

    Add parameter checks in tests

--- tests/testthat/test-boost_model.R
+++ tests/testthat/test-boost_model.R
@@ -1,7 +1,38 @@
 
+# Test Inputs
+test_that(
+  "Test for boost_model(): y is a variable in the train data",
+  {
+    data(diabetes)
+    expect_error(boost_model("Diabetes", diabetes))
+  }
+)
 
 test_that(
-  "The boost_model() runs with test data",
+  "Test for boost_model(): y is a variable in the test data",
+  {
+    data(diabetes)
+    sample <- sample(c(1, 2), nrow(diabetes), replace = TRUE, prob = c(.8, .2))
+    train <- diabetes[sample == 1, 1:4]
+    test <- diabetes[sample == 2, 2:4]
+    expect_error(boost_model("Diabetes_binary", train, test))
+  }
+)
+
+test_that(
+  "Test for boost_model(): y is a variable in the test data",
+  {
+    data(diabetes)
+    sample <- sample(c(1, 2), nrow(diabetes), replace = TRUE, prob = c(.8, .2))
+    train <- diabetes[sample == 1, 1:4]
+    test <- diabetes[sample == 2, 1:5]
+    expect_error(boost_model("Diabetes_binary", train, test))
+  }
+)
+
+# Test Outputs
+test_that(
+  "Test for boost_model(): runs with test data",
   {
     data(diabetes)
     sample <- sample(c(1, 2), nrow(diabetes), replace = TRUE, prob = c(.8, .2))
@@ -14,7 +45,7 @@ test_that(
 )
 
 test_that(
-  "The boost_model() runs with no test data",
+  "Test that boost_model(): runs with no test data",
   {
     data(diabetes)
     xg_fit <- boost_model("Diabetes_binary", diabetes)
