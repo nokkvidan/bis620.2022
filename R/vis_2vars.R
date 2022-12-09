c83427a9cee6adead22a2bd44e472a5c5763f48f
Elisa Loy <37096340+loye16@users.noreply.github.com>
2022-12-09 04:02:08 -0500

    Update vis_2vars.R

--- R/vis_2vars.R
+++ R/vis_2vars.R
@@ -1,28 +1,38 @@
 #' @title vis_2vars
 #' @param d diabetes data frame
-#' @param y an arbitrary variable in diabetes data frame
 #' @param x an arbitrary variable in diabetes data frame
+#' @param y an arbitrary variable in diabetes data frame
 #' @importFrom ggplot2 ggplot geom_bar geom_violin aes
 #' @examples \dontrun{
 #' vis_2vars(df, 'Diabetes_binary', 'BMI')
 #' }
 #' @export
-vis_2vars <- function(d, y, x) {
-  col1 <- d[, x]
-  col2 <- d[, y]
-  fac1 <- length(unique(col1)) <= 15
-  fac2 <- length(unique(col2)) <= 15
+vis_2vars <- function(d, x, y) {
+  if (x %!in% colnames(train)) {
+    stop(paste0(x, " is not a variable in your data"))
+  }
+  if (y %!in% colnames(train)) {
+    stop(paste0(y, " is not a variable in your data"))
+  }
+  fac1 <- length(unique(d[, x])) <= 15
+  fac2 <- length(unique(d[, y])) <= 15
   if (fac1 && fac2) {
-    p <- ggplot(d, aes(x = eval(str2lang(x)), fill = eval(str2lang(y)))) +
-      ggplot2::geom_bar(position = "dodge")
+    d[, x] <- as.factor(d[, x])
+    d[, y] <- as.factor(d[, y])
+    p <- ggplot(d, aes(x = eval(str2lang(x)), fill = eval(str2lang(y)),
+                       group = eval(str2lang(y)))) +
+      ggplot2::geom_bar(position = "dodge") +
+      labs(fill=y)
     subtitle <- "Barplot visualization"
   } else if (fac1 && !fac2) {
+    d[, x] <- as.factor(d[, x])
     p <- ggplot(d) +
       ggplot2::geom_violin(aes(x = eval(str2lang(x)), y = eval(str2lang(y))))
     subtitle <- "Violinplot visualization"
   } else if (!fac1 && fac2) {
+    d[, y] <- as.factor(d[, y])
     p <- ggplot(d) +
-      ggplot2::geom_violin(aes(x = eval(str2lang(x)), y = eval(str2lang(x))))
+      ggplot2::geom_violin(aes(x = eval(str2lang(x)), y = eval(str2lang(y))))
     subtitle <- "Violinplot visualization"
   } else if (!fac1 && !fac2) {
     p <- ggplot(d) +
