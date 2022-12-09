01026666bed1c4b0c11e517c251c8b4f34950441
Elisa Loy <37096340+loye16@users.noreply.github.com>
2022-12-09 04:02:12 -0500

    Update test-vis_num.R

--- tests/testthat/test-vis_num.R
+++ tests/testthat/test-vis_num.R
@@ -1,7 +1,17 @@
 
-
+# Test inputs
 test_that(
-  "The vis_num() runs with no errors",
+  "Test for vis_num(): check for data frame with only binary columns",
+  {
+    data(diabetes)
+    df_dist <- diabetes[, c("Diabetes_binary", "Sex")]
+    vis_num(df_dist)
+  }
+)
+
+# Test for covr, devtools:test() skipped due to lack of output
+test_that(
+  "Test for vis_num(): overall test of function",
   {
     data(diabetes)
     vis_num(diabetes)
