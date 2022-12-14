fac340bd95623e5b3f127de36b6c7b5770024cbb
Alex Han <ahan98@gmail.com>
2022-12-13 21:28:26 -0500

    formula wrapper for categorical variables

--- R/glm_model.R
+++ R/glm_model.R
@@ -1,3 +1,5 @@
+source("~/bis620.2022/R/string.R")
+
 #' @description Fits a GLM to the input data and returns the model object
 #' @title glm_model
 #' @param y character response variable
@@ -62,15 +64,19 @@ glm_model <- function(y, train, test = NULL, optimize = NA) {
     sig <- which(sum$coef[, 4] < 0.1)[-1]
     sigvars <- rownames(sum$coefficients)[sig]
     # Then re run the model on significant variables only
-    form1 <- as.formula(paste("Diabetes_binary", "~",
-                              paste(sigvars, collapse = "+")))
+    # form1 <- as.formula(paste("Diabetes_binary", "~",
+    #                           paste(sigvars, collapse = "+")))
+    form1 <- as.formula(paste(y, "~",
+                              paste(sapply(sigvars, split_string), collapse="+")))
+    print(form1)
     fit1 <- glm(form1, data = train, family = binomial)
     # Save this new model's summary and then rerun with 0.05 significance
     sum1 <- summary(fit1)
     sig1 <- which(sum1$coef[, 4] < 0.05)[-1]
     sigvars1 <- rownames(sum1$coefficients)[sig1]
-    form2 <- as.formula(paste("Diabetes_binary", "~",
-                              paste(sigvars1, collapse = "+")))
+    # form2 <- as.formula(paste("Diabetes_binary", "~",
+    #                           paste(sigvars1, collapse = "+")))
+    form2 <- as.formula(paste(y, "~", paste(sigvars1, collapse = "+")))
     # This is now the final model
     final_fit <- glm(form2, data = train, family = binomial)
   } else if (optimize == "stepAIC") {
@@ -78,8 +84,7 @@ glm_model <- function(y, train, test = NULL, optimize = NA) {
     step <- MASS::stepAIC(fit)
     sigvars <- names(step$coefficients[-1])
     # Rerun the glm
-    form2 <- as.formula(paste("Diabetes_binary", "~",
-                              paste(sigvars, collapse = "+")))
+    form2 <- as.formula(paste(y, "~", paste(sigvars, collapse = "+")))
     final_fit <- glm(form2, data = train, family = binomial)
   }
   # If testing data is provided:
---
+++ R/string.R
@@ -0,0 +1,12 @@
+split_string <- function(string) {
+  firstNumber <- sub(".*?(\\d).*", "\\1", string)
+  if (nchar(firstNumber) > 1) {
+    # no number in string
+    return( string )
+  }
+  
+  indexOfFirstNumber <- unlist(gregexpr(firstNumber, string))
+  prefix <- substring(string, 1, indexOfFirstNumber - 1)
+  suffix <- substring(string, indexOfFirstNumber)
+  return( paste0("I(", prefix, "==", suffix, ")") )
+}
