81d3249ca2ffef563a3ac10f2fcb59f214071058
Alex Han <ahan98@gmail.com>
2022-12-21 23:59:42 -0500

    fix counterfactual

--- R/get_counterfactual.R
+++ R/get_counterfactual.R
@@ -20,51 +20,61 @@
 #' @examples \dontrun{
 #' data(diabetes)
 #' rf_fit <- rf_model("Diabetes_binary", diabetes, optimize = TRUE)
-#' counterfactual <- get_counterfactual(rf_fit[[1]], diabetes, TRUE)
+#' counterfactual <- get_counterfactual(rf_fit[[1]], diabetes)
 #' }
 #' @export
 get_counterfactual <- function(model, data) {
 
   binary_factors <- c("HeartDiseaseorAttack", "HighBP", "HighChol",
                       "PhysActivity")
-  levels <- c(1, 1, 1, 0)  # starting unhealthy levels
+  binary_levels <- c(1, 1, 1, 0)  # unhealthy levels
 
   # nolint start
   # helper function to predict diabetic proportion
   predict_diabetic_proportion <- function(model, data) {
     if (inherits(model, "glm")) {
-      type <- "response"
+      preds <- predict(model, data, type = "response")
     } else if (inherits(model, "randomForest")) {
-      type <- "prob"
+      preds <- predict(model, data, type = "prob")[, "1"]
     } else if (inherits(model, "xgb.Booster")) {
       matrix_formula <- as.formula("Diabetes_binary ~ .")
       data <- Matrix::sparse.model.matrix(matrix_formula, data)[, -1]
-      type <- "prob"
+      preds <- predict(model, data, type = "prob")
     } else {
       stop("Model must inherit from randomForest, xgb.Booster, or glm")
     }
-    return(mean(predict(model, data, type) > 0.5))
+    return(mean(preds > 0.5))
   }
   # nolint end
 
-  # get proportion of diabetic population, subset with each binary factor
-  subsets <- purrr::map2(binary_factors, levels, ~subset(data, get(.x) == .y),
-                         subset(data, get("Diabetes_binary") == 1))
-  dataframes <- append(list(data), subsets)
+  # get proportion of diabetic population, subset with unhealthy variable levels
+  diabetic <- subset(data, get("Diabetes_binary") == 1)
+  binary_subsets <- purrr::map2(binary_factors, binary_levels,
+                                ~ subset(diabetic, get(.x) == .y))
+  bmi_subset <- subset(diabetic, (get("BMI") < 15) | (get("BMI") > 17))
+  dataframes <- append(list(bmi_subset), binary_subsets)
   before <- purrr::map_dbl(dataframes, ~ predict_diabetic_proportion(model, .x))
 
-  # get proportion of diabetic population, subset with opposite level of
-  # each binary factor
-  levels <- purrr::map_int(levels, ~ !.x)
-  subsets <- purrr::map2(binary_factors, levels, ~ subset(data, get(.x) == .y),
-                         subset(data, get("Diabetes_binary") == 1))
-  data[, "BMI"] <- purrr::map_dbl(data[, "BMI"],
-                                  ~ if (.x < 15) return(15)
-                                  else if (.x > 17) return(17)
-                                  else return(.x))
-  dataframes <- append(list(data), subsets)
+  # get proportion of diabetic population, subset with healthy variable levels
+  # change to healthy levels for binary factors
+  binary_subsets <- purrr::map(seq_along(binary_factors), function(i) {
+    var <- binary_factors[i]
+    old_level <- factor(binary_levels[i])
+    new_level <- factor(0 + !binary_levels[i])
+    df <- binary_subsets[[i]]
+    df[, var] <- sapply(df[, var], replace, old_level, new_level)
+    return(df)
+  })
+
+  # change to healthy BMI range
+  bmi_subset[, "BMI"] <- purrr::map_dbl(bmi_subset[, "BMI"],
+                                        ~ if (.x < 15) return(15)
+                                        else if (.x > 17) return(17)
+                                        else return(.x))
+  dataframes <- append(list(bmi_subset), binary_subsets)
   after <- purrr::map_dbl(dataframes, ~ predict_diabetic_proportion(model, .x))
 
+  # build counterfactual table
   counterfactual <- data.frame(Before = before,
                                After = after,
                                Change = after - before)
--- doc/bis620.R
+++ doc/bis620.R
@@ -1,9 +1,106 @@
 ## ---- include = FALSE---------------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
-  comment = "#>"
+  comment = "#"
 )
 
-## ----setup--------------------------------------------------------------------
+## ----setup, message = FALSE---------------------------------------------------
 library(bis620.2022)
+library(dplyr)
+
+# Load the data
+data(diabetes)
+
+# Splitting the dataset for train and test
+set.seed(1)
+sample <- sample(c(1, 2), nrow(diabetes), replace = TRUE, prob = c(.8, .2))
+
+# convert all variables to categorical, except for Age and BMI
+train <- diabetes[sample == 1, ] %>%
+  mutate_at(vars(-c("Age", "BMI")), as.factor)
+test <- diabetes[sample == 2, ] %>%
+  mutate_at(vars(-c("Age", "BMI")), as.factor)
+
+## ----vis2, warnings = FALSE, fig.width=7, fig.height=7------------------------
+# Factor v continuous produces a violin plot
+# Continuous v factor also produces a violin plot
+p1 <- vis_2vars(diabetes, "Diabetes_binary", "BMI")
+# Continuous v continuous produces a scatter plot
+p2 <- vis_2vars(diabetes, "MentHlth", "BMI")
+# Factor v factor produces a side by side bar plot
+p3 <- vis_2vars(diabetes, "Diabetes_binary", "Sex")
+p4 <- vis_2vars(diabetes, "Age", "Diabetes_binary")
+gridExtra::grid.arrange(p1, p2, p3, p4)
+
+## ----visnum, fig.width=7------------------------------------------------------
+p5 <- vis_num(diabetes)
+
+## ----visdist, fig.width=7, fig.height = 6-------------------------------------
+df_dist <- diabetes[, c("BMI", "MentHlth", "Sex")]
+vis_dist(df_dist)
+
+## ---- eval = FALSE------------------------------------------------------------
+#  # Fit a GLM to the diabetes data
+#  glm_fit0 <- glm_model("Diabetes_binary", diabetes)
+#  # Fit a random forest model to the diabetes data
+#  rf_fit0 <- rf_model("Diabetes_binary", diabetes)
+#  # Fit an XGBoost model to the diabetes data
+#  xg_fit0 <- boost_model("Diabetes_binary", diabetes)
+
+## ---- results = "hide", fig.show = "hide", message = FALSE, warning = FALSE----
+# Fit and evaluate the performance of the GLM model
+glm_fit <- glm_model("Diabetes_binary", train, test, optimize = "manual")
+# Fit and evaluate the performance of the random forest model
+rf_fit <- rf_model("Diabetes_binary", train, test, optimize = TRUE)
+# Fit and evaluate the performance of the XGBoost model
+xg_fit <- boost_model("Diabetes_binary", train, test)
+
+## ---- results = "hide", fig.show = "hide", message = FALSE, warning = FALSE----
+glm_fit <- glm_model("Diabetes_binary", diabetes, optimize = "manual")
+
+## -----------------------------------------------------------------------------
+glm_fit <- glm_model("Diabetes_binary", train, test, optimize = "manual")
+summary(glm_fit)
+
+## ---- fig.width=7, fig.height=7-----------------------------------------------
+p1 <- vis_2vars(diabetes, "Diabetes_binary", "HighBP")
+p2 <- vis_2vars(diabetes, "Diabetes_binary", "HighChol")
+p4 <- vis_2vars(diabetes, "Sex", "Diabetes_binary")
+p3 <- vis_2vars(diabetes, "Diabetes_binary", "BMI")
+gridExtra::grid.arrange(p1, p2, p3, p4)
+
+## ---- results = "hide", fig.show = "hide", message = FALSE, warning = FALSE----
+rf_fit <- rf_model("Diabetes_binary", train, test, optimize = TRUE)
+
+## -----------------------------------------------------------------------------
+print(rf_fit[[1]])
+rf_fit[[6]]
+
+## -----------------------------------------------------------------------------
+library(randomForest, include.only = "importance")
+importance(rf_fit[[1]])
+rf_fit[[7]]
+
+## ---- results = "hide", fig.show = "hide", message = FALSE, warning = FALSE----
+xg_fit <- boost_model("Diabetes_binary", train, test)
+
+## -----------------------------------------------------------------------------
+print(xg_fit[[1]])
+print(xg_fit[[2]])
+rf_fit[[7]]
+
+## -----------------------------------------------------------------------------
+print(xg_fit[[3]])
+
+## -----------------------------------------------------------------------------
+# xg boost counterfactual
+get_counterfactual(model = xg_fit[[1]], data = test)
+
+## -----------------------------------------------------------------------------
+# random forest counterfactual
+get_counterfactual(model = rf_fit[[1]], data = test)
+
+## -----------------------------------------------------------------------------
+# glm counterfactual
+get_counterfactual(model = glm_fit[[1]], data = test)
 
